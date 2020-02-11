d4e592b2b70b054119520a726b4f5d44405e2a80
jramirez36 <42725506+jramirez36@users.noreply.github.com>
2020-02-11 08:56:45 -0600

    segundo push

---
+++ programacion/funciones/prueba.js
@@ -0,0 +1,36 @@
+function greet() 
+{
+    console.log(`hi pero de function`);
+}
+//
+// function de flecha
+// function expression es cuando una variable le asigno una funcion como abajo
+let greet1 = () => {
+    console.log(`hi de variable que tiene una function`);
+}
+greet1();
+//function are first-class es cuando puedo pasar una function como variable
+function logGreenting(fn)
+{
+    fn();
+}
+logGreenting(greet);
+let logGreenting1 = (fn) => 
+{
+    fn();
+}
+logGreenting1(greet1);
+// mandando una function por completo
+//function expression on the fly
+logGreenting1(() => {
+    console.log(`hi`);
+});
+
+// comprobado que `` se ocupa para concatenar con ${}
+function suma()
+{
+    let a = 10;
+    console.log(`prueba de por que usar ${a}`);
+}
+suma();
+// `${a}` forma de concatenar actual
