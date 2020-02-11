7c7a3f33d6257b59acf0847de34518c654fe327b
jramirez36 <42725506+jramirez36@users.noreply.github.com>
2020-02-11 07:15:57 -0600

    primer subida

---
+++ programacion/app.js
@@ -0,0 +1,5 @@
+var carNames = require('car-names');
+var allNames = carNames.all;
+var randomName = carNames.random();
+//console.log(carNames); todos los nombres
+console.log(randomName); // un nombre aleatorio
